c652029682586c72b00feb87603015e79c4c637e
SEKI-YUTA <zard.since1991@gmail.com>
2021-03-21 21:03:17 +0900

    finished foundation

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const serviceWrapper = document.querySelector('.bl_services_wrapper');
-const services = serviceWrapper.querySelectorAll('.el_service');
+const serviceWrapper = document.querySelector('.bl_blocks_wrapper');
+const services = serviceWrapper.querySelectorAll('.el_animItem');
 const animBg = serviceWrapper.querySelector('.bl_animBg')
 console.log(animBg);
 services.forEach((service) => {
